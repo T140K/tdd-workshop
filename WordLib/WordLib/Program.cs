47e47daf1ab01e3df2dacb2c54220e9f89e96fcc
Darek <darek1430143@gmail.com>
2023-06-13 18:47:55 +0200

    basic test with hardcoded act

--- WordLib/WordLib/Program.cs
+++ WordLib/WordLib/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WordLib
 {
-    public class Class1
+    public class Program
     {
     }
 }
--- WordLib/WordLibTests/Tests.cs
+++ WordLib/WordLibTests/Tests.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordLib;
+using Xunit;
 
 namespace WordLibTests
 {
-    public class Class1
+    public class Tests
     {
+        /*
+            if 0 words, 1 space or 1 letter return flase because its not a word
+        */
+
+
+        [Fact]
+        public void IsPallindromeTest()
+        {
+            //Arrange
+            string myString = "lol";
+
+            //Act
+            bool isPalindrome = myString.SequenceEqual(myString.Reverse());
+
+
+            //Assert
+            Assert.True(isPalindrome);
+
+            
+        }
     }
 }
